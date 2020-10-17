3223e454a4bd8838c22be13862baabae327baf1d
LasVegas <dodaaa@bk.ru>
2020-10-18 02:43:02 +0300

    Проверка работы autodeploy сервера, изменил версию - 1.1.0

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
           создать тестовый запрос на сервер автодеплоя
         </button>
       </div>
-      <p class="subtitle">version 1.0.37</p>
+      <p class="subtitle">version 1.1.0</p>
     </div>
   </div>
 </template>
