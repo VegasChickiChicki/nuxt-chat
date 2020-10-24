b17b092796bf38954f83a073f8c7cdff1dd8b5d6
LasVegas <dodaaa@bk.ru>
2020-10-24 19:03:53 +0300

    Мелкие правки стилей. Поменял версию 1.2.12

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,15 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;700&display=swap'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap'
+      }
     ]
   },
 
